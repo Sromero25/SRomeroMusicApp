61264efb97c6b47890dd66e3558cbf4cda1466e6
Saulo Romero <saulofco25@gmail.com>
2026-05-13 16:10:25 -0600

    Color y Tema

--- app/src/main/java/com/example/sromeromusicapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/sromeromusicapp/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.sromeromusicapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80    = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PurplePrimary = Color(0xFF7B4FFF)
+val PurpleLight   = Color(0xFFEDE7FF)
+val PurpleDark    = Color(0xFF2D1B69)
+val DarkPlayer    = Color(0xFF1A0A3D)
+val CardWhite     = Color(0xFFFFFFFF)
+val TextPrimary   = Color(0xFF1A1A2E)
+val TextSecondary = Color(0xFF6B6B8A)
